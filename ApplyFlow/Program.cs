d2aa9b890273cbc6ea52e8551d9139dfb3754d97
Bernard Kaminski <bernardkaminski.bk@gmail.com>
2026-06-09 17:57:53 +0200

    updated compose file

--- ApplyFlow/Program.cs
+++ ApplyFlow/Program.cs
@@ -22,7 +22,8 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins(
             "http://localhost:5173",
-            "http://localhost:4173"
+            "http://localhost:4173",
+            "http://localhost:3000"
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
